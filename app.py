5b268f8aa36fc60421cadf2f2d26496a3a9f2261
Ashlē Mercer <103979087+ashlemercer@users.noreply.github.com>
2022-07-14 20:32:48 -0500

    last commit.

--- app.py
+++ app.py
@@ -9,14 +9,16 @@ app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
 mongo = PyMongo(app)
 
 @app.route("/")
+
 def index():
    mars = mongo.db.mars.find_one()
    return render_template("index.html", mars=mars)
 
 @app.route("/scrape")
+
 def scrape():
    mars = mongo.db.mars
-   mars_data = scraping.scrape_all()
+   mars_data = scraping1.scrape_all()
    mars.update_one({}, {"$set":mars_data}, upsert=True)
    return redirect('/', code=302)
 
